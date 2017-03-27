a7ecee669440907deb9bb42a12097c5ab37bc692
Axel Venet <kern046@gmail.com>
2017-03-27 07:47:51 +0200

    Implement applicative container for asylamba game

--- applications/asylamba-game/docker-entrypoint.sh
+++ applications/asylamba-game/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ DEPLOY_SOURCES != false ]; then
+if [ $DEPLOY_SOURCES == true ]; then
     rm -rf /srv/apps/asylamba-game
     tar xf /srv/archives/asylamba_game.tar.gz -C /srv/apps/
 fi
