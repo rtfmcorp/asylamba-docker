ee3398424af0de7094fd8f326c9d8ca9fd023d4b
Axel Venet <kern046@gmail.com>
2017-06-01 00:36:21 +0200

    Implement crontabs for the game rankings

--- applications/asylamba-game/docker-entrypoint.sh
+++ applications/asylamba-game/docker-entrypoint.sh
@@ -4,5 +4,8 @@ if [ $DEPLOY_SOURCES == true ]; then
     rm -rf /srv/apps/asylamba-game
     tar xf /srv/archives/asylamba_game.tar.gz -C /srv/apps/
 fi
+rm -rf /var/spool/cron/crontabs/root
+eval "echo \"$(cat /etc/cron.d/ranking_cron)\"" > /var/spool/cron/crontabs/root
+rm /etc/cron.d/ranking_cron
 
 exec "$@"
