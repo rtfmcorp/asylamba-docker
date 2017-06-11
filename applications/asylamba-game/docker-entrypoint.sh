3d95d400bd488b6743cc0fa8f665b852eb5a420a
Axel Venet <kern046@gmail.com>
2017-06-11 13:44:53 +0200

    Remove crons from game container

--- applications/asylamba-game/docker-entrypoint.sh
+++ applications/asylamba-game/docker-entrypoint.sh
@@ -4,9 +4,5 @@ if [ $DEPLOY_SOURCES == true ]; then
     rm -rf /srv/apps/asylamba-game
     tar xf /srv/archives/asylamba_game.tar.gz -C /srv/apps/
 fi
-rm -rf /var/spool/cron/crontabs/root
-eval "echo \"$(cat /etc/cron.d/ranking_cron)\"" > /var/spool/cron/crontabs/root
-rm /etc/cron.d/ranking_cron
-cp /var/spool/cron/crontabs/root /etc/cron.d/ranking_cron
 
 exec "$@"
