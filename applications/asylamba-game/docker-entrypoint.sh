595456d85a784999d4764bdbb0079cbb0d6e4ca8
Axel Venet <kern046@gmail.com>
2017-06-13 13:26:15 +0200

    Fix CWD issue

--- applications/asylamba-game/docker-entrypoint.sh
+++ applications/asylamba-game/docker-entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+cd /
 mkdir -p /srv/apps
 if [ $DEPLOY_SOURCES == true ]; then
     rm -rf /srv/apps/asylamba-game
     tar xf /srv/archives/asylamba_game.tar.gz -C /srv/apps/
 fi
 
-if [ ! -d "/srv/apps/asylamba-game/vendor" ]; then
+if [ ! -d /srv/apps/asylamba-game/vendor ]; then
     composer install -d /srv/apps/asylamba-game
 fi
 
